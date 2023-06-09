6bdfd4ba1b04f82122b59cd9aa83283f62eac665
Vinicios Neves <vinny@macbook.lan>
2023-06-09 17:42:02 +0100

    Aula 3: video 3.4 - desafio das Tags

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Banner from "./componentes/Banner"
 import bannerBackground from './assets/banner.png'
 import Galeria from "./componentes/Galeria"
 
+import fotos from './fotos.json'
+import { useState } from "react"
+
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   width: 100%;
@@ -29,7 +32,8 @@ const ConteudoGaleria = styled.section`
   flex-grow: 1;
 `
 
-function App() {
+const App = () => {
+  const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
   return (
     <FundoGradiente>
       <EstilosGlobais />
@@ -42,7 +46,7 @@ function App() {
               texto="A galeria mais completa de fotos do espaço!"
               backgroundImage={bannerBackground}
             />
-            <Galeria />
+            <Galeria fotos={fotosDaGaleria}/>
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
---
+++ src/componentes/Galeria/Imagem/index.jsx
@@ -0,0 +1,15 @@
+const Imagem = ({ foto }) => {
+    return (<figure>
+        <img src="" alt="" />
+        <figcaption>
+            <h3>titulo</h3>
+            <footer>
+                <p>fonte</p>
+                <button>Favorito</button>
+                <button>Expandir</button>
+            </footer>
+        </figcaption>
+    </figure>)
+}
+
+export default Imagem
