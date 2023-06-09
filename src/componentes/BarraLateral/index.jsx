f423a1d2693088cbeed9846c25a9fe050ccb583b
Vinicios Neves <vinny@macbook.lan>
2023-06-09 15:25:38 +0100

    Aula 2: video 2.2

---
+++ src/componentes/BarraLateral/index.jsx
@@ -0,0 +1,31 @@
+import { styled } from "styled-components"
+
+const ListaEstilizada = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    width: 212px;
+`
+
+const BarraLateral = () => {
+    return (
+        <aside>
+            <nav>
+                <ListaEstilizada>
+                    <li>
+                        <a href="">
+                            Início
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Mais curtidos
+                        </a>
+                    </li>
+                </ListaEstilizada>
+            </nav>
+        </aside>
+    )
+}
+
+export default BarraLateral
