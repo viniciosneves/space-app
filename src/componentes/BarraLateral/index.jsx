56d6ff89804ba2422c443dc807e03f284f55231b
Vinicios Neves <vinny@macbook.lan>
2023-06-09 15:42:46 +0100

    Aula 2: video 2.3

--- src/componentes/BarraLateral/index.jsx
+++ src/componentes/BarraLateral/index.jsx
@@ -1,4 +1,5 @@
 import { styled } from "styled-components"
+import ItemNavegacao from "./ItemNavegacao"
 
 const ListaEstilizada = styled.ul`
     list-style: none;
@@ -12,16 +13,19 @@ const BarraLateral = () => {
         <aside>
             <nav>
                 <ListaEstilizada>
-                    <li>
-                        <a href="">
-                            Início
-                        </a>
-                    </li>
-                    <li>
-                        <a href="">
-                            Mais curtidos
-                        </a>
-                    </li>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/home-ativo.png" 
+                        iconeInativo="/icones/home-inativo.png"
+                        ativo={true}
+                    >
+                        Início
+                    </ItemNavegacao>
+                    <ItemNavegacao 
+                        iconeAtivo="/icones/mais-vistas-ativo.png" 
+                        iconeInativo="/icones/mais-vistas-inativo.png"
+                    >
+                        Mais vistas
+                    </ItemNavegacao>
                 </ListaEstilizada>
             </nav>
         </aside>
