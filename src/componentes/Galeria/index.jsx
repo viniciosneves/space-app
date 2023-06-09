62571864d7fe8ae69d0ce83660b68eb450856e30
Vinicios Neves <vinny@macbook.lan>
2023-06-09 17:13:22 +0100

    Aula 3: video 3.2

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -1,6 +1,28 @@
+import { styled } from "styled-components"
+import Titulo from "../Titulo"
+import Tags from "./Tags"
+import Populares from "./Populares"
+
+const GaleriaContainer = styled.div`
+    display: flex;
+`
+
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`
+
 const Galeria = () => {
     return (
-        <h2>Navegue pela galeria</h2>
+        <>
+            <Tags />
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+
+                </SecaoFluida>
+                <Populares />
+            </GaleriaContainer>
+        </>
     )
 }
 
