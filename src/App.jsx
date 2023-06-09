1285d7fb92caf7a4498b71a6566329173fd49edb
Vinicios Neves <vinny@macbook.lan>
2023-06-09 14:59:25 +0100

    Aula 2: video 2.1 + desafio do Campo de Texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { styled } from "styled-components"
+import EstilosGlobais from "./componentes/EstilosGlobais"
+import Cabecalho from "./componentes/Cabecalho"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -11,7 +13,8 @@ function App() {
 
   return (
     <FundoGradiente>
-
+      <EstilosGlobais />
+      <Cabecalho />
     </FundoGradiente>
   )
 }
