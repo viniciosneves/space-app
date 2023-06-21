7ba05ad72cd6ced3805e438977cf95b594358fb5
Vinicios Neves <vinny@macbook.lan>
2023-06-21 20:43:19 +0100

    desafio: rodape

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Galeria from "./componentes/Galeria"
 import fotos from './fotos.json'
 import { useState } from "react"
 import ModalZoom from "./componentes/ModalZoom"
+import Rodape from "./componentes/Rodape"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -72,6 +73,7 @@ const App = () => {
           </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
+      <Rodape />
       <ModalZoom 
         foto={fotoSelecionada}
         aoFechar={() => setFotoSelecionada(null)}
